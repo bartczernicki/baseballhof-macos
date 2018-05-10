c8d8f01ad7012396152c11ebac471ac2ba270d4a
bartczernicki <bartczernicki@gmail.com>
2018-05-09 23:22:23 -0400

    Fix structure.

--- MachineLearningBaseBallHOF/Program.cs
+++ MachineLearningBaseBallHOF/Program.cs
@@ -10,53 +10,53 @@ namespace MachineLearningBaseBallHOF
 {
     class Program
     {
-		static void Main(string[] args)
-		{
+        static void Main(string[] args)
+        {
             var trainingDataPath = "HOFTraining.txt";
             var validationDataPath = "HOFValidation.txt";
 
-			Console.WriteLine("Starting Baseball HOF Training");
+            Console.WriteLine("Starting Baseball HOF Training");
 
-			// 1) Create a new learning pipeline
-			var pipeline = new LearningPipeline();
-            
-			// 2) Add a Text Loader
-			pipeline.Add(new TextLoader<BaseballData>(trainingDataPath, separator: ","));
+            // 1) Create a new learning pipeline
+            var pipeline = new LearningPipeline();
 
-			// 3) Create Features
-			pipeline.Add(new ColumnConcatenator("Features", "YearsPlayed", "AB", "H", "AllStarAppearances", "MVPs"));
+            // 2) Add a Text Loader
+            pipeline.Add(new TextLoader<BaseballData>(trainingDataPath, separator: ","));
 
-			// 4) Create new classifier
-			//var classifier = new FastTreeBinaryClassifier() { NumLeaves = 5, NumTrees = 5, MinDocumentsInLeafs = 2 };
-			//var classifier = new BinaryLogisticRegressor();
-			var classifier = new FastForestBinaryClassifier();
+            // 3) Create Features
+            pipeline.Add(new ColumnConcatenator("Features", "YearsPlayed", "AB", "H", "AllStarAppearances", "MVPs"));
 
-			pipeline.Add(classifier);
+            // 4) Create new classifier
+            //var classifier = new FastTreeBinaryClassifier() { NumLeaves = 5, NumTrees = 5, MinDocumentsInLeafs = 2 };
+            //var classifier = new BinaryLogisticRegressor();
+            var classifier = new FastForestBinaryClassifier();
 
-			// 5) Train Model
-			var model = pipeline.Train<BaseballData, BaseballDataPrediction>();
+            pipeline.Add(classifier);
 
-			// 6) Sample Prediction
-			var samplePrediction = new BaseballData
-			{
-				AB = 100,
-				AllStarAppearances = 3,
-				FullPlayerName = "dfd",
-				H = 400,
-				ID = 3343,
-				Label = false,
-				MVPs = 1,
-				YearsPlayed = 10
-			};
-			var result = model.Predict(samplePrediction);
+            // 5) Train Model
+            var model = pipeline.Train<BaseballData, BaseballDataPrediction>();
 
-			// 7) Load Evaluation Data
-			var testData = new TextLoader<BaseballData>(validationDataPath, useHeader: false, separator: ",");
+            // 6) Sample Prediction
+            var samplePrediction = new BaseballData
+            {
+            	AB = 100,
+            	AllStarAppearances = 3,
+            	FullPlayerName = "dfd",
+            	H = 400,
+            	ID = 3343,
+            	Label = false,
+            	MVPs = 1,
+            	YearsPlayed = 10
+            };
+            var result = model.Predict(samplePrediction);
 
-			// 8) Evaluate trained model with test data
-			var evaluator = new BinaryClassificationEvaluator() { ProbabilityColumn = "Probability" };
-			var metrics = evaluator.Evaluate(model, testData);
-            
+            // 7) Load Evaluation Data
+            var testData = new TextLoader<BaseballData>(validationDataPath, useHeader: false, separator: ",");
+
+            // 8) Evaluate trained model with test data
+            var evaluator = new BinaryClassificationEvaluator() { ProbabilityColumn = "Probability" };
+            var metrics = evaluator.Evaluate(model, testData);
+
             // 9) Print out Metrics (rounded to 4 decimals)
             Console.WriteLine("******************");
             Console.WriteLine("Evaluation Metrics");
@@ -67,8 +67,8 @@ namespace MachineLearningBaseBallHOF
             Console.WriteLine("Accuracy:   " + Math.Round(metrics.Accuracy, 4).ToString());
             Console.WriteLine("******************");
 
-			//10) Persist trained model
-			model.WriteAsync("baseballhof-model.mlnet").Wait();
-		}
+            //10) Persist trained model
+            model.WriteAsync("baseballhof-model.mlnet").Wait();
+        }
     }
 }
