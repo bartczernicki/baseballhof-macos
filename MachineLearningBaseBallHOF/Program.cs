d5cafa173e405bfb38ad29c0a4bc5aae7541594d
bartczernicki <bartczernicki@gmail.com>
2018-05-09 23:19:30 -0400

    Changes to program structure.

--- MachineLearningBaseBallHOF/Program.cs
+++ MachineLearningBaseBallHOF/Program.cs
@@ -12,28 +12,31 @@ namespace MachineLearningBaseBallHOF
     {
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Starting Baseball HOF Training");
-            
-			var pipeline = new LearningPipeline();
-
 			var trainingDataPath = "HOFTraining.txt";
-			var validationDataPath = "HOFValidation.txt";
+            var validationDataPath = "HOFValidation.txt";
 
+			Console.WriteLine("Starting Baseball HOF Training");
+
+			// 1) Create a new learning pipeline
+			var pipeline = new LearningPipeline();
+            
+			// 2) Add a Text Loader
 			pipeline.Add(new TextLoader<BaseballData>(trainingDataPath, separator: ","));
 
+			// 3) Create Features
 			pipeline.Add(new ColumnConcatenator("Features", "YearsPlayed", "AB", "H", "AllStarAppearances", "MVPs"));
 
+			// 4) Create new classifier
 			//var classifier = new FastTreeBinaryClassifier() { NumLeaves = 5, NumTrees = 5, MinDocumentsInLeafs = 2 };
 			//var classifier = new BinaryLogisticRegressor();
 			var classifier = new FastForestBinaryClassifier();
 
 			pipeline.Add(classifier);
 
+			// 5) Train Model
 			var model = pipeline.Train<BaseballData, BaseballDataPrediction>();
-            
 
-			var testData = new TextLoader<BaseballData>(validationDataPath, useHeader: false, separator: ",");
-
+			// 6) Sample Prediction
 			var samplePrediction = new BaseballData
 			{
 				AB = 100,
@@ -45,20 +48,27 @@ namespace MachineLearningBaseBallHOF
 				MVPs = 1,
 				YearsPlayed = 10
 			};
-
 			var result = model.Predict(samplePrediction);
 
+			// 7) Load Evaluation Data
+			var testData = new TextLoader<BaseballData>(validationDataPath, useHeader: false, separator: ",");
+
+			// 8) Evaluate trained model with test data
 			var evaluator = new BinaryClassificationEvaluator() { ProbabilityColumn = "Probability" };
 			var metrics = evaluator.Evaluate(model, testData);
+            
+			// 9) Print out Metrics (rounded to 4 decimals)
+			Console.WriteLine("******************");
+			Console.WriteLine("Evaluation Metrics");
+			Console.WriteLine("******************");
+			Console.WriteLine("AUC Score:  " + Math.Round(metrics.Auc, 4).ToString());
+			Console.WriteLine("Precision:  " + Math.Round(metrics.PositivePrecision, 4).ToString());
+			Console.WriteLine("Recall:     " + Math.Round(metrics.PositiveRecall, 4).ToString());
+			Console.WriteLine("Accuracy:   " + Math.Round(metrics.Accuracy, 4).ToString());
+            Console.WriteLine("******************");
 
-			Console.WriteLine("*****************");
-			Console.WriteLine("AUC Score: " + metrics.Auc.ToString());
-			Console.WriteLine("Precision: " + metrics.PositivePrecision.ToString());
-			Console.WriteLine("Recall:    " + metrics.PositiveRecall.ToString());
-			Console.WriteLine("Accuracy:  " + metrics.Accuracy.ToString());
-			Console.WriteLine("*****************");
-
-			Console.ReadLine();
+			//10) Persist trained model
+			model.WriteAsync("baseballhof-model.mlnet").Wait();
 		}
     }
 }
